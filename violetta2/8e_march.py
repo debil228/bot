1cedefaadee0a860a7f5cf6cb188e5b1da853c46
debil228 <31446926+debil228@users.noreply.github.com>
2023-03-06 23:30:43 +0600

    Update 8e_march.py

--- violetta2/8e_march.py
+++ violetta2/8e_march.py
@@ -37,6 +37,6 @@ executor.start_webhook(
     on_startup=on_startup,
     on_shutdown=on_shutdown,
     skip_updates=True,
-    host="0.0.0.0",
+    host="94.142.141.146",
     port=int(os.environ.get("PORT", 22))
 )
